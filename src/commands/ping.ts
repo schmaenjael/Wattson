041dc27494910b5e9f72c0c8fa975e7a75db4c22
schmaenjael <mayer.manuel@protonmail.ch>
2025-03-31 17:07:58 +0200

    dev: add bot setup

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,10 +1,10 @@
-import { SlashCommandBuilder, type Interaction, type CacheType } from 'discord.js';
+import { SlashCommandBuilder, type ChatInputCommandInteraction, type CacheType } from 'discord.js';
 import { SlashCommand } from '~/models';
 
 const data = new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!');
 
-const execute = async (interaction: Interaction<CacheType>) => {
-  await interaction.user.send('Pong!');
+const execute = async (interaction: ChatInputCommandInteraction<CacheType>) => {
+  await interaction.reply('Pong!');
 };
 
 export default { data, execute } as SlashCommand;
